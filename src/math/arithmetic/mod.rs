468975910088a502ebe23e1d8253c4098d098e07
ImQWERTY39 <sharans.roy@gmail.com>
2024-03-01 20:21:51 +0530

    basic parser

--- src/math/arithmetic/mod.rs
+++ src/math/arithmetic/mod.rs
@@ -1,3 +1,7 @@
+const VALID_TOKENS: [char; 14] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/',
+];
+
 #[derive(Debug)]
 pub enum ArithmeticExpression {
     Number(f64),
@@ -18,27 +22,63 @@ impl ArithmeticExpression {
         }
     }
 
-    pub fn parse(expression: &str) -> Self {
-        let expr = tokenize(expression);
-
-        if expr.is_empty() {
-            panic!("Emptry expression");
-        }
-
-        // match get_lowest_precedence(&expr) {
-        //     "+" => Self::Add(Box::new()),
-        // }
-    }
+    // pub fn parse(expression: &str) -> Self {
+    //     let expr = tokenize(expression);
+    //     parser(&expr)
+    // }
 }
 
-fn tokenize(expression: &str) -> Vec<&str> {
+fn tokenize(expression: &str) -> Vec<String> {
     let mut tokenized = Vec::new();
 
+    let mut digit = String::new();
+    let mut in_digit = false;
+
     for i in expression.chars() {
-        if i.is_digit(10) {}
+        if i.is_whitespace() {
+            continue;
+        }
+
+        if !VALID_TOKENS.contains(&i) {
+            panic!("Invalid token");
+        }
+
+        if i.is_ascii_digit() {
+            if !in_digit {
+                in_digit = true;
+            }
+
+            digit.push(i);
+            continue;
+        }
+
+        if in_digit {
+            tokenized.push(digit.clone());
+
+            digit = String::new();
+            in_digit = false;
+        }
+
+        tokenized.push(i.to_string());
+    }
+
+    if !digit.is_empty() {
+        tokenized.push(digit);
     }
 
     tokenized
 }
 
 // fn get_lowest_precedence<'a>(expr: &'a [&str]) -> &'a str {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let tokenised1 = tokenize("5 * 2 + 3");
+        let tokenised1: Vec<&str> = tokenised1.iter().map(|x| x.as_str()).collect();
+        assert_eq!(tokenised1, vec!["5", "*", "2", "+", "3"]);
+    }
+}
